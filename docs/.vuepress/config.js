55d5971dafe8fbcd3cc05cf6f60b2e8661fb00fc
zhengkaidi <zhengkaidi@keep.com>
2019-10-16 15:35:23 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,21 @@ module.exports = {
   title: "Hello VuePress",
   description: "Just playing around",
   themeConfig: {
-    sidebar: ["/", "/page-a", ["/page-b", "Explicit link text"]]
+    sidebar: {
+      "/guide/": [
+        {
+          title: "入门",
+          collapsable: false,
+          children: [
+            // 子菜单
+            "install/test" // 可在docs目录下创建install目录，此路径表示读取install目录下的README.md文件
+          ]
+        }
+      ]
+    }
+  },
+  markdown: {
+    // markdown 插入代码时展示对应行数
+    lineNumbers: true
   }
 };
---
+++ docs/.vuepress/dist/assets/js/10.d474c111.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{7:function(n,w,o){}}]);
